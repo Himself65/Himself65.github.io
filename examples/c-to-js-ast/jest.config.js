fd6482ce4f2555c4f4256fc29f1354b5a47aa062
himself65 <himself65@outlook.com>
2020-04-25 10:09:57 +0800

    remove: js-to-c ast example

--- examples/c-to-js-ast/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/ts-jest'
-  },
-  transformIgnorePatterns: [
-    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$'
-  ]
-}
--- examples/c-to-js-ast/src/parser.ts
+++
@@ -1,5 +0,0 @@
-type AST = unknown
-
-export function parser (code: string): AST {
-  return code
-}
