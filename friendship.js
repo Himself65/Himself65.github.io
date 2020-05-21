50423531e303d407101f17b228dbe9d98370768c
himself65 <himself65@outlook.com>
2020-05-21 19:37:09 +0800

    feat: add a friend '兔子鮮笙'

--- friendship.js
+++ friendship.js
@@ -3,9 +3,10 @@ const push = (name, url, image = '', description = '') =>
   friendship.push({ name, url, image, description })
 
 push('ice1000', 'https://ice1000.org', 'ice1000.jpg')
-push('Edward Elric', 'https://sasuke40.github.io/', 'sasuke.jpg')
-push('DIYgod', 'https://diygod.me/', 'DIYgod.jpg')
+push('Edward Elric', 'https://sasuke40.github.io', 'sasuke.jpg')
+push('DIYgod', 'https://diygod.me', 'DIYgod.jpg')
 push('太狼', 'https://lynvv.xyz', 'lynvv.jpg')
 push('木子', 'https://blog.k8s.li', 'muzi502.png')
+push('兔子鮮笙', 'https://blog.tuzi.moe', 'ryuuuuu.png')
 
 module.exports = friendship
