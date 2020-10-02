a50618ba9d349e68f82de252c507eb5247ce69a2
himself65 <himself65@outlook.com>
2020-10-02 12:53:32 +0800

    fix: remove query type in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,11 @@ import '../style/index.css'
 
 import React from 'react'
 
-import type { IndexPageQuery } from '~types'
-
 import Layout from '../components/layout'
 import ProfileCard from '../components/ProfileCard'
 import SEO from '../components/seo'
 
-const BlogIndex: React.FC<{ data: IndexPageQuery }> = () => {
+const BlogIndex: React.FC = () => {
   return (
     <Layout title='' brief>
       <SEO title='Home'/>
