5c00fc8d5e95f96474f5549ce66cf8791ace0553
himself65 <himself65@outlook.com>
2020-10-13 21:49:24 +0800

    fix: typography.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,22 @@
 import './global.css'
 
 import Typography from 'typography'
+import Wordpress2016 from 'typography-theme-wordpress-2016'
 
-const typography = new Typography()
+Wordpress2016.overrideThemeStyles = () => ({
+  blockquote: {
+    color: 'inherit',
+    borderLeftColor: 'inherit',
+    opacity: '0.8'
+  }
+})
+
+const typography = new Typography(Wordpress2016)
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== 'production') {
+  typography.injectStyles()
+}
 
 export default typography
 export const rhythm = typography.rhythm
