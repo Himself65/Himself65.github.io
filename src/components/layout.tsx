ee5b480bb7b8d824ffabb93232a84d65cc892c92
himself65 <himself65@outlook.com>
2021-04-06 11:43:01 +0800

    feat: add webpushr support

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -124,7 +124,11 @@ const Layout: React.FC<{
               content: theme === 'light' ? '#ffa8c5' : '#282c35'
             }
           ]}
-        />
+        >
+          <script>
+            {'(function(w,d, s, id) {if(typeof(w.webpushr)!==\'undefined\') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";fjs.parentNode.appendChild(js);}(window,document, \'script\', \'webpushr-jssdk\'));webpushr(\'setup\',{\'key\':\'BBwI_4scackYl7TwZKw4IPx3aVZXIy3g1XG6fmjG-0TkIvUv9qpwTNgzuCpUzJTayWhSgzgsDRDNQ8NyKiVVjgQ\' });'}
+          </script>
+        </Helmet>
         <header
           style={{
             display: 'flex',
