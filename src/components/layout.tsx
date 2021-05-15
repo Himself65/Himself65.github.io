60307b000d7e3c68a181205cd1cd882002f6eedd
himself65 <himself65@outlook.com>
2021-05-15 14:57:19 +0800

    feat: add google adsense script

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -161,6 +161,7 @@ const Layout: React.FC<{
             }
           ]}
         >
+          <script data-ad-client='ca-pub-9648629100084933' async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'/>
           <script>
             {'(function(w,d, s, id) {if(typeof(w.webpushr)!==\'undefined\') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";fjs.parentNode.appendChild(js);}(window,document, \'script\', \'webpushr-jssdk\'));webpushr(\'setup\',{\'key\':\'BBwI_4scackYl7TwZKw4IPx3aVZXIy3g1XG6fmjG-0TkIvUv9qpwTNgzuCpUzJTayWhSgzgsDRDNQ8NyKiVVjgQ\' });'}
           </script>
