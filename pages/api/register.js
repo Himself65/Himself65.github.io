fd1ccb58aec58dbefa15df103c797251e39d817e
himself65 <himself65@outlook.com>
2021-06-29 02:46:35 +0800

    feat: update `/api/register` with verify challenge

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,11 +1,27 @@
-import { createPrivateKey, createSign, randomBytes } from 'crypto'
+import { createPrivateKey, createSign, createVerify, randomBytes } from 'crypto'
 
+const privateKeyPem = process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
+
+/**
+ *
+ * @param req {NextApiRequest}
+ * @param res {NextApiResponse}
+ */
 export default async function (req, res) {
-  const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
-  const keys = await createPrivateKey(privateKey)
-  // get the random challenge from server
-  const challenge = createSign('SHA256').update(randomBytes(36)).sign(keys)
-  res.status(200).json({
-    challenge,
-  })
+  const privateKey = await createPrivateKey(privateKeyPem)
+  if (!req.body.challenge) {
+    // get the random challenge from server
+    const challenge = randomBytes(18)
+    const sign = createSign('SHA256').update(challenge).sign(privateKey)
+    res.status(200).json({
+      challenge: Buffer.concat([challenge, sign]),
+    })
+  } else {
+    const { challenge } = req.body
+    const buf = Buffer.from(challenge)
+    if (createVerify('SHA256').update(buf.slice(0, 15)).verify(privateKey, buf.slice(16))) {
+      // is correct challenge
+      res.status(200)
+    }
+  }
 }
