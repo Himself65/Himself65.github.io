2de99e7587bcf12d2b66ff9ea020a20a80d97806
himself65 <himself65@outlook.com>
2021-06-28 22:04:29 +0800

    deps: use react 18

--- next.config.js
+++ next.config.js
@@ -5,18 +5,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      // Replace React with Preact only in client production build
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
-
-    return config
-  },
   async rewrites() {
     return {
       beforeFiles: [
