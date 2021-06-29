a42d02c821267b0e277f917abc46b17c92d3794b
himself65 <himself65@outlook.com>
2021-06-29 12:58:55 +0800

    feat: update `/api/register`

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,27 +1,61 @@
-import { createPrivateKey, createSign, createVerify, randomBytes } from 'crypto'
+import { NextApiResponse, NextApiRequest } from 'next'
+import { Fido2Lib } from 'fido2-lib'
+import { withIronSession, Session } from 'next-iron-session'
 
 const privateKeyPem = process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
+const fido2Lib = new Fido2Lib({
+  rpId: 'himself65.com',
+  cryptoParams: [-7],
+  authenticatorUserVerification: 'required',
+})
 
 /**
  *
- * @param req {NextApiRequest}
+ * @param req {NextApiRequest & { session: Session }}
  * @param res {NextApiResponse}
  */
-export default async function (req, res) {
-  const privateKey = await createPrivateKey(privateKeyPem)
-  if (!req.body.challenge) {
-    // get the random challenge from server
-    const challenge = randomBytes(18)
-    const sign = createSign('SHA256').update(challenge).sign(privateKey)
-    res.status(200).json({
-      challenge: Buffer.concat([challenge, sign]),
-    })
-  } else {
-    const { challenge } = req.body
-    const buf = Buffer.from(challenge)
-    if (createVerify('SHA256').update(buf.slice(0, 15)).verify(privateKey, buf.slice(16))) {
-      // is correct challenge
-      res.status(200)
+export default withIronSession(
+  async function (req, res) {
+    if (!req.body.data && req.method === 'GET') {
+      const assertionOptions = await fido2Lib.assertionOptions()
+
+      console.log(assertionOptions)
+      req.session.set('webauthn', {
+        challenge: Buffer.from(assertionOptions.challenge),
+      })
+      await req.session.save()
+
+      res.status(200).json({
+        ...assertionOptions,
+        challenge: Buffer.from(assertionOptions.challenge),
+      })
+    } else if (req.method === 'POST') {
+      const { challenge: expectedChallenge } = req.session.get('webauthn')
+      const {
+        data: {
+          id,
+          response: { clientDataJSON, attestationObject },
+        },
+      } = req.body
+      const attestation = {
+        id: Buffer.from(id),
+        response: {
+          clientDataJSON: `${clientDataJSON}`,
+          attestationObject: `${attestationObject}`,
+        },
+      }
+      const result = await fido2Lib.attestationResult(attestation, {
+        challenge: Buffer.from(expectedChallenge).toString('base64'),
+        origin: 'himself65.com',
+        factor: 'either',
+      })
     }
+  },
+  {
+    password: privateKeyPem,
+    cookieName: 'webauthn',
+    cookieOptions: {
+      secure: process.env.NODE_ENV === 'production',
+    },
   }
-}
+)
